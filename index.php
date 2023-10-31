77f8a57402fed3dbacc053fed32f53032fae2b87
Kacper Milewski <khaghorpl@gmail.com>
2023-10-31 12:25:57 +0100

    blok met w wersji desktop

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ get_header();
 
 <main>
     <h1 class="page-title">Strona główna Gate 61 Offices</h1>
+    <?php include get_theme_file_path().'/src/blocks/met.php'; ?>
 </main>
 
 <?php
---
+++ src/blocks/met.php
@@ -0,0 +1,21 @@
+<div id="met" class="met">
+    <div class="met-banner">
+        <div class="met-img">
+            <img src="<?= wp_get_attachment_image_src(35, 'full')[0] ?>">
+        </div>
+        <div class="met-text">
+            <h2><?php _e('Poznaj Gate 61', 'gate') ?></h2>
+            <p>
+                <?php _e('Budynek biurowy przy', 'gate') ?>
+                <span><?php _e('Al. Krakowskiej 61 w nowej odsłonie', 'gate') ?></span>
+            </p>
+        </div>
+    </div>
+    <div class="met-content">
+        <p>
+            Funkcjonalny, dostosowany do potrzeb, pracowników i biznesu, w pobliżu Lotniska Chopina i Centrum Okęcie Park. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin sit amet dignissim tellus.
+            <strong><?php _e('Zapraszamy na pokład!', 'gate')?></strong>
+        </p>
+        <a href="#contact" class="met-contact btn btn-primary--red"><?php _e('Napisz do nas', 'gate') ?></a>
+    </div>
+</div>
