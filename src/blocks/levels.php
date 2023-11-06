282555158be698213c3551ddf6cf980b9e5ebe87
Kacper Milewski <khaghorpl@gmail.com>
2023-11-06 13:46:17 +0100

    poprawka dla bloku levels

--- src/blocks/levels.php
+++ src/blocks/levels.php
@@ -1,40 +1,12 @@
 <div id="levels" class="levels tabs">
     <h2 class="levels-title"><?php _e('Rzuty pięter', 'gate') ?></h2>
     <nav class="levels-nav tabs-nav js-tab">
-        <button class="btn btn-outline tabs-item active" data-target="level_2"><?php _e('Piętro 2', 'gate') ?></button>
-        <button class="btn btn-outline tabs-item" data-target="level_3"><?php _e('Piętro 3', 'gate') ?></button>
-        <button class="btn btn-outline tabs-item" data-target="level_4"><?php _e('Piętro 4', 'gate') ?></button>
+        <button class="btn btn-outline tabs-item active" data-target="level_3_1"><?php _e('Piętro 3', 'gate') ?></button>
+        <button class="btn btn-outline tabs-item" data-target="level_3_2"><?php _e('Piętro 3', 'gate') ?></button>
         <button class="btn btn-outline tabs-item" data-target="level_5"><?php _e('Piętro 5', 'gate') ?></button>
     </nav>
     <div class="levels-wrapper">
-        <div id="level_2" class="levels-item active">
-            <div class="levels-header">
-                <h3><?php _e('Piętro 2', 'gate') ?></h3>
-                <ul>
-                    <li>
-                        <?php _e('Powierchnia netto: ', 'gate') ?>
-                        <span><?php _e('5654,45 sqm', 'gate') ?></span>
-                    </li>
-                    <li>
-                        <?php _e('Powierchnia brutto: ', 'gate') ?>
-                        <span><?php _e('6095,48 sqm', 'gate') ?></span>
-                    </li>
-                    <li>
-                        <?php _e('Dostępna powierchnia: ', 'gate') ?>
-                        <span><?php _e('6095,48 sqm', 'gate') ?></span>
-                    </li>
-                </ul>
-            </div>
-            <div class="map">
-                <img src="<?= wp_get_attachment_image_src(65, 'full')[0] ?>"></img>
-            </div>
-            <div class="buttons">
-                <button class="btn btn-primary">+</button>
-                <button class="btn btn-primary">-</button>
-            </div>
-        </div>
-
-        <div id="level_3" class="levels-item">
+        <div id="level_3_1" class="levels-item active">
             <div class="levels-header">
                 <h3><?php _e('Piętro 3', 'gate') ?></h3>
                 <ul>
@@ -53,17 +25,17 @@
                 </ul>
             </div>
             <div class="map">
-                <img src="<?= wp_get_attachment_image_src(65, 'full')[0] ?>"></img>
+                <img src="<?= wp_get_attachment_image_src(68, 'full')[0] ?>"></img>
             </div>
             <div class="buttons">
-                <button class="">+</button>
-                <button class="">-</button>
+                <button class="btn btn-primary">+</button>
+                <button class="btn btn-primary">-</button>
             </div>
         </div>
 
-        <div id="level_4" class="levels-item">
+        <div id="level_3_2" class="levels-item">
             <div class="levels-header">
-                <h3><?php _e('Piętro 4', 'gate') ?></h3>
+                <h3><?php _e('Piętro 3', 'gate') ?></h3>
                 <ul>
                     <li>
                         <?php _e('Powierchnia netto: ', 'gate') ?>
@@ -80,7 +52,7 @@
                 </ul>
             </div>
             <div class="map">
-                <img src="<?= wp_get_attachment_image_src(65, 'full')[0] ?>"></img>
+                <img src="<?= wp_get_attachment_image_src(67, 'full')[0] ?>"></img>
             </div>
             <div class="buttons">
                 <button class="">+</button>
@@ -107,7 +79,7 @@
                 </ul>
             </div>
             <div class="map">
-                <img src="<?= wp_get_attachment_image_src(65, 'full')[0] ?>"></img>
+                <img src="<?= wp_get_attachment_image_src(66, 'full')[0] ?>"></img>
             </div>
             <div class="buttons">
                 <button class="">+</button>
